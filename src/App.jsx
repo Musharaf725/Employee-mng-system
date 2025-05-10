c3bd295e177ae703f8412d935acc41e7cf96862f
Musharaf Ali <alimusharaf725@gmail.com>
2025-05-10 16:54:46 +0530

    New Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
-import EmpDashboard from './components/EmpDashboard'
-import AdminDashboard from './components/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
+  const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  const [userData,SetUserData] = useContext(AuthContext)
+
   useEffect(()=>{
-    // setLocalStorage()
-    getLocalStorage()
-  },)
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
+    if(loggedInUser){
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    }
+
+  },[])
+
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+      }
+    }
+    else {
+      alert("Invalid Credentials")
+    }
+  }
+
+
+
   return (
     <>
-    <Login/>
-    {/* <EmpDashboard /> */}
-    {/* <AdminDashboard/> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
     </>
   )
 }
